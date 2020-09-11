a5a4cd0a3e6e7f243ee17dadb9ee551d82e454b3
Van Yang <vanyang@iisigroup.com>
2020-09-11 17:01:26 +0800

    set sleep time

--- clock.py
+++ clock.py
@@ -8,14 +8,21 @@ import urllib.request
 sched = BlockingScheduler()
 
 # @sched.scheduled_job('interval', day_of_week='mon-fri', minutes=20)
-@sched.scheduled_job('cron', day_of_week='mon-fri', minute='*/20')
+@sched.scheduled_job('cron', day_of_week='0-6', hour='15-23', minute='*/20')
 def timed_job():
-    # url = "https://van-linebot.herokuapp.com/"
-    # conn = urllib.request.urlopen(url)
-    # cost down aonther app
+    # wake myself
     url2 = "https://herokualarm.herokuapp.com/"
     conn = urllib.request.urlopen(url2)
     today = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-    print(today, 'This job is run every work day twenty minutes.')
+    print(today, 'This job is run every day twenty minutes.')
+
+@sched.scheduled_job('cron', day_of_week='0-6', hour=23, minute=59, second=59)
+def work_in_shifts():
+	# wake anther app before sleep
+    url = "https://van-linebot.herokuapp.com/"
+    conn = urllib.request.urlopen(url)
+    today = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    print(today, 'This job is wake aonther server every day at 07:59:59 UTC+8.')
+
 
 sched.start()
