24421e273eaa18a7a0b67bfcf0128e27fac8500a
Van Yang <vanyang@iisigroup.com>
2020-09-11 15:45:34 +0800

    add call myself

--- clock.py
+++ clock.py
@@ -12,9 +12,9 @@ sched = BlockingScheduler()
 def timed_job():
     url = "https://van-linebot.herokuapp.com/"
     conn = urllib.request.urlopen(url)
-    # cost down aonther app
-    # url2 = "https://mingchang.herokuapp.com/home/"
-    # conn = urllib.request.urlopen(url2)
+    cost down aonther app
+    url2 = "https://herokualarm.herokuapp.com/"
+    conn = urllib.request.urlopen(url2)
     today = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     print(today, 'This job is run every work day twenty minutes.')
 
